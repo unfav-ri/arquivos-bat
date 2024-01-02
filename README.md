6d45c87493025729618e5b1251abe93c68386f33
just Rick <133035218+unfav-ri@users.noreply.github.com>
2024-01-01 22:17:20 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# arquivos-bat
-Alguns codigos em bat para passar o tempo.
+# Arquivos .bat Personalizados
+
+Este repositório contém vários scripts .bat personalizados para automação e utilidades no ambiente Windows.
+
+---
+
+## Descrição dos Arquivos
+
+### 1. Barra de Progresso (barra_de_progresso.bat)
+- Um script simples para simular uma barra de progresso no terminal.
+
+### 2. Disk Check (disk_check.bat)
+- Um script para verificar o estado do disco e apresentar informações relacionadas ao armazenamento.
+
+### 3. Login (login.bat)
+- Um script para simular um processo de login básico.
+
+### 4. Matrix (matrix.bat)
+- Um script que cria um efeito visual semelhante ao código "Matrix" em uma janela de terminal.
+
+### 5. Piscando (piscando.bat)
+- Um script que faz piscar a tela.
+
+### 6. Info do Sistema (info_do_sistema.bat)
+- Um script para coletar e exibir informações básicas sobre o sistema operacional e hardware.
+
+### 7. Terra (terra.bat)
+- Um script que simula uma visualização da terra girando no terminal.
+
+### 8. TV (tv.bat)
+- Um script que simula uma tela de TV sem sinal no terminal.
+
+---
+
+## Como Utilizar
+
+Clone este repositório:
+
+1- abra o terminal
+
+2- git clone https://github.com/unfav-ri/arquivos-bat.git
+
+3- cd arquivos-bat
+
+4- "script".bat
+
+---
---
+++ tv.bat
@@ -0,0 +1,31 @@
+::TV sem sinal
+@echo OFF
+Title TV
+mode con cols=80 lines=25
+color f
+cls
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[47m                       [40m         FORA DO AR           [47m                       
+echo.[43m                             [41m                   [43m                             
+pause >nul
